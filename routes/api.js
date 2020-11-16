5fa0f0527cf340d5230d69ce985ced9641d0cb8d
JCJ <jesse.jackson.c@gmail.com>
2020-11-15 20:02:58 -0600

    adding get route for workout

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,17 @@
 const router = require("express").Router();
+const { db } = require("../models/workoutSchema");
 const Workout = require("../models/workoutSchema");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
 
 router.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
